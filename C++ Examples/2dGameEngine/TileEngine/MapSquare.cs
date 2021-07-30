26e55c801f023b58d84e0ad2308ee0b4b3b0bf0e
TangerineMoss9 <77404972+TangerineMoss9@users.noreply.github.com>
2021-07-30 17:21:37 -0400

    Add files via upload

---
+++ C++ Examples/2dGameEngine/TileEngine/MapSquare.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TileEngine
+{
+    
+    [Serializable]
+    public class MapSquare
+    {
+        #region Declarations
+        
+
+        public int[] LayerTiles = new int[4];   
+        public string CodeValue = "";           
+        public bool Passable = true;            
+
+        
+        #endregion
+
+
+
+        #region Constructor
+        
+
+        public MapSquare(int background, int interactive, int foreground, int parallax, string code, bool passable)
+        {
+            LayerTiles[0] = background;     
+            LayerTiles[1] = interactive;    
+            LayerTiles[2] = foreground;
+            LayerTiles[3] = parallax;
+            CodeValue = code;               
+            Passable = passable;            
+        }
+
+        
+        #endregion
+
+
+
+        #region Public Methods
+        
+
+        public void TogglePassable()
+        {
+            Passable = !Passable;  
+        }
+
+        
+        #endregion
+
+
+    }
+}
