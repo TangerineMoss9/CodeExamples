db8e787d1c03e8de82156793ace04918918aa18a
TangerineMoss9 <77404972+TangerineMoss9@users.noreply.github.com>
2021-07-30 17:11:01 -0400

    Add files via upload

---
+++ API-and-Algorithms/Quest6/Quest6/EnemySprite.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Quest6
+{
+    class EnemySprite : Sprite
+    {
+        bool b = true;
+        public Vector2 dir;
+
+        int x;
+        int y;
+
+        Random r = new Random();
+
+        public enum GameState { Fleeing,Cruising };
+
+        public GameState currentGameState = GameState.Cruising;
+        // Sprite is automated. Direction is same as speed
+        public override Vector2 direction
+        {
+            get { return speed; }
+
+        }
+
+
+        //Constructor
+        public EnemySprite(Texture2D textureImage, Vector2 position, Point frameSize,
+            int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed)
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame,
+            sheetSize, speed)
+        {
+        }
+
+        //Constructor using ms Per Frame
+        public EnemySprite(Texture2D textureImage, Vector2 position, Point frameSize,
+            int collisionOffset, Point currentFrame, Point sheetSize, Vector2 speed,
+            int millisecondsPerFrame)
+            : base(textureImage, position, frameSize, collisionOffset, currentFrame,
+            sheetSize, speed, millisecondsPerFrame)
+        {
+        }
+
+
+
+        public override void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            // Move sprite based on direction
+            if (b)
+            {
+                dir = direction;
+                b = false;
+            }
+
+            position += dir;
+
+            if (position.X < 0)
+            {
+                position.X = 0;
+                if (currentGameState == GameState.Fleeing)
+                {
+                    currentGameState = GameState.Cruising;
+
+                    x = r.Next(5, 11) / 5;
+                    y = r.Next(-10, 11) / 5;
+
+                    while (y == 0)
+                    {
+                        y = r.Next(-10, 11) / 5;
+
+                    }
+
+                    dir = new Vector2(x, y);
+                }
+                else
+                {
+                    dir.X *= -1;
+                }
+
+            }
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+                if (currentGameState == GameState.Fleeing)
+                {
+                    currentGameState = GameState.Cruising;
+
+                    x = r.Next(-10, 11) / 5;
+                    y = r.Next(5, 11) / 5;
+
+                    while (x == 0)
+                    {
+                        x = r.Next(-10, 11) / 5;
+
+                    }
+
+                    dir = new Vector2(x, y);
+                }
+                else
+                {
+                    dir.Y *= -1;
+                }
+            }
+            if (position.X > clientBounds.Width - frameSize.X)
+            {
+                position.X = clientBounds.Width - frameSize.X;
+                if (currentGameState == GameState.Fleeing)
+                {
+                    currentGameState = GameState.Cruising;
+
+                    x = r.Next(-10, -5) / 5;
+                    y = r.Next(-10, 11) / 5;
+
+                    while (x == 1)
+                    {
+                        y = r.Next(-10, 11) / 5;
+
+                    }
+
+                    dir = new Vector2(x, y);
+                }
+                else
+                {
+                    dir.X *= -1;
+                }
+            }
+
+            if (position.Y > clientBounds.Height - frameSize.Y)
+            {
+                position.Y = clientBounds.Height - frameSize.Y;
+                if (currentGameState == GameState.Fleeing)
+                {
+                    currentGameState = GameState.Cruising;
+
+                    x = r.Next(-10, 11) / 5;
+                    y = r.Next(-10, -5) / 5;
+
+                    while (x == 0)
+                    {
+                        x = r.Next(-10, 11) / 5;
+
+                    }
+
+                    dir = new Vector2(x, y);
+                }
+                else
+                {
+                    dir.Y *= -1;
+                }
+            }
+
+
+            //Call base update (Sprite class)
+            base.Update(gameTime, clientBounds);
+        }
+    }
+}
---
+++ API-and-Algorithms/Quest6/Quest6/Game1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+
+namespace Quest6
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        //Pointers to Graphics 
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        int x;
+        int y;
+        float NyDir;
+        float NxDir;
+
+
+
+        Random r = new Random();
+        
+        
+       
+
+
+        //NEW-> Connects and Manages our Sprite classes (calls Updates/Draws)
+        public SpriteManager spriteManager;
+
+        
+
+        public Game1()
+        {
+            //Typical instantiation and settings
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            //Typical instantiation
+            spriteManager = new SpriteManager(this);
+
+            //NEW ->This connects us to the SpriteManager class
+            Components.Add(spriteManager);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            for (int i = 0; i < 20; i++) { 
+            x = r.Next(0, 500);
+            y = r.Next(0, 500);
+            NxDir = r.Next(1, 20) / 10;
+            NyDir = r.Next(1, 20) / 10;
+            spriteManager.spawn(x, y, NxDir, NyDir);
+            }
+
+
+
+
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+
+            //if (respawnTime >= 2)
+            //{
+            //    x = r.Next(0, 500);
+            //    y = r.Next(0, 500);
+            //    NxDir = r.Next(1, 20) / 10;
+            //    NyDir = r.Next(1, 20) / 10;
+
+            //    spriteManager.spawn(x, y, NxDir, NyDir);
+
+            //    respawnTime = 0;
+            //}
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //Clear the screen
+            GraphicsDevice.Clear(Color.White);
+            
+            //Base draw handles all base work for drawing
+            base.Draw(gameTime);
+        }
+    }
+}
